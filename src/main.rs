ff93a46474f486207eb8b548106e288a037b47ca
Nicolas Hubner <nicolas.hubner@etu.u-bordeaux.fr>
2024-02-29 15:18:18 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,21 @@ fn inner(elem: Option<ElementRef>) -> String {
     elem.map(|e| e.inner_html()).unwrap_or(String::from(""))
 }
 
-fn main() {
-    let args = Args::parse();
+struct Menu {
+    date: String,
+    meals: Vec<Meal>
+}
+struct Meal {
+    style: String,
+    food: String
+}
 
-    let result = reqwest::blocking::get(args.url)
+fn get_menus(url: String) -> Vec<Menu> {
+    let result = reqwest::blocking::get(url)
         .and_then(|resp| resp.text());
     if result.is_err() {
         eprintln!("Error fetching data from url");
-        return;
+        return Vec::new();
     }
     let response = result.unwrap();
     let document = Html::parse_document(&response);
@@ -35,26 +42,42 @@ fn main() {
     //let meal_title_selector = Selector::parse("div.meal_title").unwrap();
     let meal_foodies_li_selector = Selector::parse("ul.meal_foodies>li").unwrap();
     let li_selector = Selector::parse("li").unwrap();
-    let mut days = 1;
+    let mut menus = Vec::new();
     for menu in document.select(&menu_selector) {
         let date = inner(menu.select(&date_selector).next());
-        println!("{}{}{}", color::Bg(color::Blue), date, color::Bg(color::Reset));
-        for meal in menu.select(&meal_selector) {
-            //let meal_title = inner(meal.select(&meal_title_selector).next());
-            //println!("  {}", meal_title);
-            for foodies in meal.select(&meal_foodies_li_selector) {
-                print!("  {}{}{}: ", color::Fg(color::Green), foodies.text().next().unwrap_or(""), color::Fg(color::Reset));
-                let foods = foodies.select(&li_selector)
-                    .map(|e| e.inner_html())
-                    .map(|str| str.replace(".", "").trim().to_string())
-                    .collect::<Vec<String>>()
-                    .join(", ");
-                println!("{}", foods);
-                //for food in foodies.select(&li_selector) {
-                //    print!("{}, ", food.inner_html().replace(".", "").trim());
-                //}
-                //println!();
-            }
+        let meal = menu.select(&meal_selector).next().unwrap();
+        //let meal_title = inner(meal.select(&meal_title_selector).next());
+        let mut meals = Vec::new();
+        for foodies in meal.select(&meal_foodies_li_selector) {
+            let style = foodies.text().next().unwrap_or("");
+            let food = foodies.select(&li_selector)
+                .map(|e| e.inner_html())
+                .map(|str| str.replace(".", "").trim().to_string())
+                .collect::<Vec<String>>()
+                .join(", ");
+            meals.push(Meal {
+                style: style.to_string(),
+                food
+            });
+        }
+        menus.push(Menu {
+            date,
+            meals
+        });
+    } 
+    menus
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let menus = get_menus(args.url);
+    let mut days = 1;
+    for menu in menus {
+        println!("{}{}{}", color::Bg(color::Blue), menu.date, color::Bg(color::Reset));
+        for meal in menu.meals {
+            print!("  {}{}{}: ", color::Fg(color::Green), meal.style, color::Fg(color::Reset));
+            println!("{}", meal.food);
         }
         days += 1;
         if days > args.days {
