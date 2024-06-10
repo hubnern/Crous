6e38c43e601058d025f07b6059cfc54aa568853d
Nicolas hubner <nicolas.hubner@u-bordeaux.fr>
2024-06-10 10:24:56 +0200

    add --list option do display a list of the available restaurants in the config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{collections::HashMap, fs};
-use std::process::exit;
-use serde::Deserialize;
-use scraper::{Html, Selector, ElementRef};
-use termion::color;
 use clap::Parser;
+use scraper::{ElementRef, Html, Selector};
+use serde::Deserialize;
+use std::process::exit;
+use std::{collections::HashMap, fs};
+use termion::color;
 
 /// Récupère les menus de restaurants crous et les affiche joliment dans le terminal
 #[derive(Parser, Debug)]
@@ -11,10 +11,14 @@ use clap::Parser;
 struct Args {
     /// Restaurants à afficher le menu. Les noms sont ceux inscrit comme alias dans la config. Si aucun restaurants n'est renseigné, le menu affiché sera celui du champ "default" de la config
     restaurants: Vec<String>,
-    
+
     /// Nombre de jours à afficher
     #[arg(short, long, default_value_t = 1)]
     days: u8,
+
+    /// Affiche les restaurants enregistrés dans la config
+    #[arg(short, long)]
+    list: bool,
 }
 
 fn inner(elem: Option<ElementRef>) -> String {
@@ -23,24 +27,27 @@ fn inner(elem: Option<ElementRef>) -> String {
 
 struct Menu {
     date: String,
-    meals: Vec<Meal>
+    meals: Vec<Meal>,
 }
 struct Meal {
     style: String,
-    food: String
+    food: String,
 }
 
 #[derive(Deserialize, Debug)]
 struct Config {
     aliases: HashMap<String, String>,
-    default: Option<String>
+    default: Option<String>,
 }
 
 impl Config {
     fn get_or_create() -> Self {
         let opt = dirs::config_dir();
         if opt.is_none() {
-            return Self { aliases: HashMap::new(), default: None }
+            return Self {
+                aliases: HashMap::new(),
+                default: None,
+            };
         }
         let mut path = opt.unwrap();
         path.push("crous");
@@ -49,31 +56,38 @@ impl Config {
             let mut dir = path.clone();
             dir.pop();
             let _ = fs::create_dir_all(dir);
-            let _ = fs::write(&path, "default=\"ru\"\n[aliases]\n    ru = \"<url_du_ru>\"\n");
+            let _ = fs::write(
+                &path,
+                "default=\"ru\"\n[aliases]\n    ru = \"<url_du_ru>\"\n",
+            );
             eprintln!("Configuration created in ~/.config/crous/crous.toml. Please set the url of your restaurant inside.");
             exit(1);
         }
-        
+
         match fs::read_to_string(path) {
             Ok(content) => match toml::from_str(&content) {
                 Ok(c) => c,
                 Err(e) => {
                     eprintln!("Error loading config: {}", e);
-                    Config { aliases: HashMap::new(), default: None }
+                    Config {
+                        aliases: HashMap::new(),
+                        default: None,
+                    }
                 }
             },
             Err(e) => {
                 eprintln!("Error reading file ~/.config/crous/crous.toml: {}", e);
-                Config { aliases: HashMap::new(), default: None }
+                Config {
+                    aliases: HashMap::new(),
+                    default: None,
+                }
             }
         }
-
     }
 }
 
 fn get_menus(url: String) -> Vec<Menu> {
-    let result = reqwest::blocking::get(url.clone())
-        .and_then(|resp| resp.text());
+    let result = reqwest::blocking::get(url.clone()).and_then(|resp| resp.text());
     if result.is_err() {
         eprintln!("Error fetching data from url {}", url);
         return Vec::new();
@@ -95,30 +109,38 @@ fn get_menus(url: String) -> Vec<Menu> {
         let mut meals = Vec::new();
         for foodies in meal.select(&meal_foodies_li_selector) {
             let style = foodies.text().next().unwrap_or("");
-            let food = foodies.select(&li_selector)
+            let food = foodies
+                .select(&li_selector)
                 .map(|e| e.inner_html())
                 .map(|str| str.replace(".", "").trim().to_string())
                 .collect::<Vec<String>>()
                 .join(", ");
             meals.push(Meal {
                 style: style.to_string(),
-                food
+                food,
             });
         }
-        menus.push(Menu {
-            date,
-            meals
-        });
-    } 
+        menus.push(Menu { date, meals });
+    }
     menus
 }
 
 fn display_menus(menus: Vec<Menu>, days: u8) {
     let mut d = 1;
     for menu in menus {
-        println!("{}{}{}", color::Bg(color::Blue), menu.date, color::Bg(color::Reset));
+        println!(
+            "{}{}{}",
+            color::Bg(color::Blue),
+            menu.date,
+            color::Bg(color::Reset)
+        );
         for meal in menu.meals {
-            print!("  {}{}{}: ", color::Fg(color::Green), meal.style, color::Fg(color::Reset));
+            print!(
+                "  {}{}{}: ",
+                color::Fg(color::Green),
+                meal.style,
+                color::Fg(color::Reset)
+            );
             println!("{}", meal.food);
         }
         d += 1;
@@ -131,6 +153,19 @@ fn display_menus(menus: Vec<Menu>, days: u8) {
 fn main() {
     let args = Args::parse();
     let config = Config::get_or_create();
+    if args.list {
+        let mut keys: Vec<&String> = config.aliases.keys().collect();
+        keys.sort();
+        for key in keys {
+            let s = if config.default.as_ref() == Some(key) {
+                " (default)"
+            } else {
+                ""
+            };
+            println!("{}{}", key, s);
+        }
+        return;
+    }
     if args.restaurants.is_empty() {
         if let Some(default) = config.default {
             if let Some(url) = config.aliases.get(&default) {
